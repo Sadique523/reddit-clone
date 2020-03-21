41b4e361e82208cd9613fdce6e692d75d72c71df
sadique523 <sadique@IOs-iMac.local>
2020-03-22 02:20:46 +0530

    checkign new feature

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Landing from "./components/landing";
 import Login from "./components/login";
 import Post from "./components/post";
 import AddPost from "./components/addPost";
+import Watch from "./components/watch";
 import "./styles.css";
 
 function App() {
@@ -12,9 +13,10 @@ function App() {
     <div>
       <BrowserRouter>
         <Switch>
-          <Route component={Landing} exact path="/" />
+          <Route component={Landing} exact path="/landing" />
           <Route component={Login} path="/login" />
           <Route component={AddPost} path="/addpost" />
+          <Route component={Watch} path="/" />
           <Route component={Post} path="/:id" />
         </Switch>
       </BrowserRouter>
